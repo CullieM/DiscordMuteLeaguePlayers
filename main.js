138d6333ad51caa488f24b9f3c503ade4457e6e8
Cullie <cullie.02@gmail.com>
2021-08-11 12:06:16 +1000

    Working (with Owlboy)

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
 import { default as config } from './config.js';
-import { Client, Guild, Intents, VoiceChannel } from 'discord.js';
+import { Client, Intents } from 'discord.js';
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
 
-client.on('messageCreate', (message) => {
-    if (message.content === '!mute') { 
-        message.member.voice.setMute(true);
-     }
-
-     const channels = message.guild.channels;
-     for (const [channelID, channel] of channels) {
-       for (const [memberID, member] of channel.members) {
-           // !IF PLAYING LEAGUE
-       }
-     }
-
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+    console.log(oldPresence.activities.toString());
+    console.log(newPresence.activities.toString());
+    if (newPresence.activities.toString() === "Owlboy")
+    {
+        newPresence.member.voice.setMute(true);
+    }
+    else if (!(newPresence.activities.toString() === "Owlboy"))
+    {
+        newPresence.member.voice.setMute(false);
+    }
 });
-
-client.on('presenceUpdate', (oldMember, newMember) => {
-    console.log("Test");
-    console.log(oldMember.activities.name);
-    console.log(newMember.activities.name);
-});
-
-
-
 client.login(config.token);
